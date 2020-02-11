5e1eb5b7aeda0b007bd62f9b15f1cd4ec8f47efb
Anthony Buonomo <anthony.r.buonomo@nasa.gov>
2020-02-11 14:20:20 -0500

    change plot with drop down request

--- app/app.py
+++ app/app.py
@@ -27,6 +27,24 @@ app.config.update(
     SC_DF=None,
     N_DF=None,
     KMEANS=None,
+    LOAD_COLS=[
+        "stem",
+        "count",
+        "scaled_counts",
+        "kmeans_cluster",
+        "manifold_x",
+        "manifold_y",
+        # "value__mean_change",
+        # "value__cid_ce__normalize_True",
+        # "value__cid_ce__normalize_False",
+        # "value__number_cwt_peaks__n_1",
+        # "value__number_cwt_peaks__n_5",
+        # "value__abs_energy",
+        # "value__absolute_sum_of_changes",
+        # "value__c3__lag_1",
+        # "value__c3__lag_2",
+        # "value__c3__lag_3",
+    ],
 )
 
 
@@ -34,40 +52,43 @@ app.config.update(
 def init():
     LOG.info(f'Reading derived time series measure from {app.config["SC_LOC"]}.')
     app.config["SC_DF"] = pd.read_csv(app.config["SC_LOC"], index_col=0)
-    LOG.info(f'Reading full keyword time series from {app.config["N_LOC"]}.')
+    LOG.info(f'Reading full stem time series from {app.config["N_LOC"]}.')
     app.config["N_DF"] = pd.read_json(app.config["N_LOC"], orient="records", lines=True)
     LOG.info(f"Reading kmeans model from {app.config['KMEANS_LOC']}")
-    app.config['KMEANS'] = joblib.load(app.config['KMEANS_LOC'])
+    app.config["KMEANS"] = joblib.load(app.config["KMEANS_LOC"])
     manifold_data = joblib.load(app.config["MAN_LOC"])
-    app.config['SC_DF']['kmeans_cluster'] = app.config['KMEANS'].labels_
-    log_count = np.log(app.config['SC_DF']['count'])
+    app.config["SC_DF"]["kmeans_cluster"] = app.config["KMEANS"].labels_
+    log_count = np.log(app.config["SC_DF"]["count"])
 
     scaler = MinMaxScaler(feature_range=(3, 10))
-    app.config['SC_DF']['scaled_counts'] = scaler.fit_transform(log_count.values.reshape(-1, 1))
-    app.config["SC_DF"]['manifold_x'] = manifold_data[:, 0]
-    app.config["SC_DF"]['manifold_y'] = manifold_data[:, 1]
+    app.config["SC_DF"]["scaled_counts"] = scaler.fit_transform(
+        log_count.values.reshape(-1, 1)
+    )
+    app.config["SC_DF"]["manifold_x"] = manifold_data[:, 0]
+    app.config["SC_DF"]["manifold_y"] = manifold_data[:, 1]
+    LOG.info(f"Ready")
 
 
 @app.route("/")
 def index():
-    chart_data = app.config["SC_DF"].to_dict(orient="records")
-    chart_data = json.dumps(chart_data, indent=2)
-    return render_template("index.html", data=chart_data)
+    return render_template("index.html")
 
 
-@app.route("/get-scatter-data", methods=['GET'])
+@app.route("/get-scatter-data", methods=["GET", "POST"])
 def get_scatter_data():
-    chart_data = app.config["SC_DF"].to_dict(orient="records")
+    in_data = request.json
+    cols = list(set(app.config["LOAD_COLS"] + [in_data["x"], in_data["y"]]))
+    chart_data = app.config["SC_DF"].loc[:, cols].to_dict(orient="records")
     return jsonify(chart_data)
 
 
 def _trans_time(ts, kwd, clus):
-    ts['year'] = [int(v[0:4]) for v in ts.index]
-    ts.columns = ['count', 'year']
+    ts["year"] = [int(v[0:4]) for v in ts.index]
+    ts.columns = ["count", "year"]
     ts = ts.reset_index(drop=True)
-    ts['keyword'] = kwd
-    ts['kmeans_cluster'] = clus
-    ts = ts.loc[:, ['keyword', 'kmeans_cluster', 'year', 'count']]
+    ts["stem"] = kwd
+    ts["kmeans_cluster"] = clus
+    ts = ts.loc[:, ["stem", "kmeans_cluster", "year", "count"]]
     ts_recs = ts.to_dict(orient="records")
     return ts_recs
 
@@ -75,17 +96,23 @@ def _trans_time(ts, kwd, clus):
 @app.route("/get-time-data", methods=["GET", "POST"])
 def get_time_data():
     data = request.json
-    ts = app.config['N_DF'].query(f'stem == \"{data["keyword"]}\"').iloc[:, 5:]
+    ts = app.config["N_DF"].query(f'stem == "{data["stem"]}"').iloc[:, 5:]
     ts = ts.T
-    ts_recs = _trans_time(ts, data['keyword'], data['kmeans_cluster'])
+    ts_recs = _trans_time(ts, data["stem"], data["kmeans_cluster"])
     return jsonify(ts_recs)
 
 
 @app.route("/get-all-time-data", methods=["GET", "POST"])
 def get_all_time_data():
-    ts = pd.DataFrame(app.config['N_DF'].iloc[:, 5:].sum())
-    ts_recs = _trans_time(ts, 'all', 0)
+    ts = pd.DataFrame(app.config["N_DF"].iloc[:, 5:].sum())
+    ts_recs = _trans_time(ts, "all", 0)
     return jsonify(ts_recs)
+
+
+@app.route("/get-all-options")
+def get_all_options():
+    opts = app.config["SC_DF"].columns.tolist()
+    return jsonify(opts)
 
 
 if __name__ == "__main__":

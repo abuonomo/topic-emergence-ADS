d3f4388f45ad41e8a95516976cbb1e983e7220ff
Anthony Buonomo <anthony.r.buonomo@nasa.gov>
2020-04-23 17:23:34 -0400

    fix version sript

--- version.py
+++ version.py
@@ -2,5 +2,5 @@ import os
 from setuptools_scm import get_version
 
 version = get_version(root=os.path.dirname(os.path.abspath(__file__)))
-version = ".".join(version.split(".")[:3])
+version = version.split('.dev')[0]
 print(version)

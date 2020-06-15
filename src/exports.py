99b7bd540a719540eadb3ec4ed2f9c13550cbf19
Anthony Buonomo <anthony.r.buonomo@nasa.gov>
2020-06-15 17:05:35 -0400

    no_viz_data

--- src/exports.py
+++ src/exports.py
@@ -13,26 +13,36 @@ LOG.setLevel(logging.INFO)
 def cli():
     pass
 
+
 @cli.command()
 @click.option("--in_slope_complex", type=Path)
 @click.option("--viz_data_loc", type=Path)
 @click.option("--filt_kwds_loc", type=Path)
 @click.option("--kwd_export_loc", type=Path)
-def keywords(in_slope_complex, viz_data_loc, filt_kwds_loc, kwd_export_loc):
+def keywords(
+    in_slope_complex,
+    viz_data_loc,
+    filt_kwds_loc,
+    kwd_export_loc,
+    include_viz_data=False,
+):
     LOG.info("Reading data")
-    sc = pd.read_csv(in_slope_complex).loc[: , ['stem', 'count']]
+    sc = pd.read_csv(in_slope_complex).loc[:, ["stem", "count"]]
 
-    with open(viz_data_loc, 'r') as f0:
+    with open(viz_data_loc, "r") as f0:
         vd = json.loads(json.load(f0))
-    t_df = pd.DataFrame(vd['token.table'])
+    t_df = pd.DataFrame(vd["token.table"])
 
-    rdf = pd.read_json(filt_kwds_loc, orient='records', lines=True)
-    skdf = rdf.loc[:, ['stem', 'keyword_list']]
+    rdf = pd.read_json(filt_kwds_loc, orient="records", lines=True)
+    skdf = rdf.loc[:, ["stem", "keyword_list"]]
 
     LOG.info("Joining data")
-    tsc = t_df.set_index('Term').join(sc.set_index('stem'))
-    kwd_df = tsc.join(skdf.set_index('stem')).reset_index()
-    kwd_df = kwd_df.rename(columns={'index': 'term'})
+    if include_viz_data is True:
+        tsc = t_df.set_index("Term").join(sc.set_index("stem"))
+    else:
+        tsc = sc.set_index("stem")
+    kwd_df = tsc.join(skdf.set_index("stem")).reset_index()
+    kwd_df = kwd_df.rename(columns={"index": "term"})
 
     LOG.info(f"Writing keywords to {kwd_export_loc}")
     kwd_df.to_csv(kwd_export_loc)

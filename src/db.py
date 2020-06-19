0d3c3011c14234536f67ef11bc22c8fb308defab
Anthony Buonomo <anthony.r.buonomo@nasa.gov>
2020-06-19 18:19:12 -0400

    comb back through, get years

--- src/db.py
+++ src/db.py
@@ -1,24 +1,24 @@
 import json
-import logging
-from pathlib import Path
-from math import ceil
+from html import unescape
+from typing import Dict, Union
 
 import click
-from html import unescape
-from extract_keywords import strip_tags
+import logging
 import spacy
+from gensim.corpora import Dictionary
+from pathlib import Path
 from spacy.lang.char_classes import ALPHA, ALPHA_LOWER, ALPHA_UPPER
 from spacy.lang.char_classes import CONCAT_QUOTES, LIST_ELLIPSES, LIST_ICONS
 from spacy.util import compile_infix_regex
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Float
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, or_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
 from textacy.ke import textrank
 from tqdm import tqdm
-from gensim.corpora import Dictionary
 
+from extract_keywords import strip_tags
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
@@ -37,7 +37,10 @@ class PaperKeywords(BASE):
         Integer, ForeignKey("keywords.id"), primary_key=True, nullable=False
     )
     score = Column(Float)
-    raw_keyword = Column(String)
+    raw_keyword = Column(
+        String
+    )  # TODO: redundant in current implementation, same as keyword.keyword
+    count = Column(Integer)
     paper = relationship("Paper", back_populates="keywords")
     keyword = relationship("Keyword", back_populates="papers")
 
@@ -54,6 +57,7 @@ class Paper(BASE):
     citation_count = Column(Integer)
     year = Column(Integer)
     nasa_affiliation = Column(Boolean)
+    lemma_text = Column(String)
     keywords = relationship("PaperKeywords", back_populates="paper")
 
     def __repr__(self):
@@ -91,13 +95,21 @@ class Keyword(BASE):
         return years
 
 
-class PaperMiner:
+def is_nu_like(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+class PaperKeywordExtractor:
     def __init__(self, nlp):
         self.nlp = nlp
         self.dct = None
 
     def extract_all_keywords(
-        self, session, normalize=None, reorder=False, batch_size=100, n_process=-1,
+        self, session, batch_size=100, n_process=-1,
     ):
         # TODO: go back through to find keywords which were missed by singlerank
         papers = session.query(Paper).all()
@@ -108,8 +120,9 @@ class PaperMiner:
 
         norm_kwds_to_now = {}
         for doc, p in pbar:
-            kwds = self.extract_keyword_from_doc(doc, normalize, reorder)
-            for kwd, score in kwds:
+            lemma_text, kwds = self.extract_keyword_from_doc(doc)
+            p.lemma_text = lemma_text
+            for kwd, score, count in kwds:
                 if kwd not in norm_kwds_to_now:
                     db_kwd = Keyword(kwd)
                     norm_kwds_to_now[kwd] = db_kwd
@@ -117,43 +130,101 @@ class PaperMiner:
                     db_kwd = norm_kwds_to_now[kwd]
                 with session.no_autoflush:
                     # Make sure not to flush when PaperKeywords has no primary keys
-                    assoc = PaperKeywords(raw_keyword=kwd, score=score)
+                    assoc = PaperKeywords(raw_keyword=kwd, score=score, count=count)
                     assoc.keyword = db_kwd
                     p.keywords.append(assoc)
 
     @staticmethod
-    def extract_keyword_from_doc(doc, normalize=None, reorder=False):
+    def extract_keyword_from_doc(doc):
         # SingleRank parameters
         kwds = textrank(
             doc,
-            normalize=normalize,
+            normalize="lemma",
             topn=999,  # This could cause issues with a huge abstract
             window_size=10,
             edge_weighting="count",
             position_bias=False,
         )
-        if reorder:
-            if normalize is None:
-                text = doc.text
-            elif normalize == "lemma":
-                text = " ".join([t.lemma_ for t in doc])
-            t = []
-            for i, (k, v) in enumerate(kwds):
-                k_inds = [(i, j) for j in range(len(text)) if text.startswith(k, j)]
-                t = t + k_inds
-            st = sorted(t, key=lambda x: x[1])
-            kwds_sorted = [kwds[i[0]] for i in st]
-            return kwds_sorted
-        else:
-            return kwds
+        # Remove keywords which are 1 character long or are numbers
+        kwds = [(k, v) for k, v in kwds if (not is_nu_like(k)) and (len(k) > 1)]
+        text = " ".join([t.lemma_.lower() for t in doc])
+        kwd_counts = [(k, v, text.count(k.lower())) for k, v in kwds]
+        return text, kwd_counts
+
+    def get_missed_keyword_locations(self, session, no_below=0, no_above=1.0):
+        """
+        For each keyword, find papers where singlerank did not identify that keyword,
+        but the keyword is present in the abstract or title.
+
+        Args:
+            session: sqlalchemy session
+            no_below: Do not find papers for keywords which appear fewer t
+                han this many times
+            no_above: Do not find papers for keywords which occur in more than this
+                fraction of the corpus
+
+        Returns:
+            list of dictionaries, each a record to be added to the PaperKeywords table.
+
+        """
+        corpus_size = session.query(Paper).count()
+        no_above_abs = int(no_above * corpus_size)
+        pkq = (
+            session.query(PaperKeywords)
+            .group_by(PaperKeywords.raw_keyword)  # TODO: use keyword_id instead?
+            .having(func.count() >= no_below)
+            .having(func.count() <= no_above_abs)
+        )
+        records = []
+        with session.no_autoflush:
+            pbar = tqdm(pkq, total=pkq.count())
+            for paper_kwd in pbar:
+                pbar.set_description(paper_kwd.raw_keyword)
+                q = (
+                    session.query(Paper)
+                    .filter(
+                        ~Paper.keywords.any(
+                            PaperKeywords.keyword_id == paper_kwd.keyword_id
+                        )
+                    )
+                    .filter(
+                        or_(
+                            Paper.title.contains(paper_kwd.raw_keyword),
+                            Paper.abstract.contains(paper_kwd.raw_keyword),
+                        )
+                    )
+                )
+                papers = q.all()
+                for p in papers:
+                    d = self.get_pk_dict(p, paper_kwd)
+                    records.append(d)
+        return records
+
+    @staticmethod
+    def get_pk_dict(p: Paper, paper_kwd: PaperKeywords) -> Dict[str, Union[str, int]]:
+        count = p.lemma_text.count(paper_kwd.raw_keyword.lower())
+        record = {
+            "raw_keyword": paper_kwd.raw_keyword,
+            "keyword_id": paper_kwd.keyword.id,
+            "paper_bibcode": p.bibcode,
+            "count": count,
+        }
+        return record
+
+
+class PaperOrganizer:
+
+    def __init__(self, no_below=5, no_above=0.5, min_mean_score=0):
+        self.no_below = no_below
+        self.no_above = no_above
+        self.min_mean_score = min_mean_score
 
     @staticmethod
     def get_year_counts(session):
         q = session.query(Paper.year, func.count(Paper.year))
         return q.group_by(Paper.year).all()
 
-    @staticmethod
-    def get_kwd_years(session, kwd):
+    def get_kwd_years(self, session, kwd):
         years = (
             session.query(Paper.year, func.count(Paper.year))
             .join(PaperKeywords)
@@ -162,34 +233,50 @@ class PaperMiner:
             .group_by(Paper.year)
             .all()
         )
-        return years
+        yd = dict(years)
+        fy = {}
+        years = [y for y, c in self.get_year_counts(session)]
+        year_min = min(years)
+        year_max = max(years)
+        for y in range(year_min, year_max):
+            if y in yd:
+                c = yd[y]
+            else:
+                c = 0
+            fy[y] = c
+        return fy
 
-    @staticmethod
-    def get_tokens(session, no_below=5, no_above=0.5):
-        q = session.query(Paper)
-        corpus_size = q.count()
-        no_above_abs = int(no_above * corpus_size)
-        q2 = (
-            session.query(Keyword, func.count(Keyword.id))
+    def get_filtered_keywords(self, session):
+        corpus_size = session.query(Paper).count()
+        no_above_abs = int(self.no_above * corpus_size)
+        kwd_query = (
+            session.query(
+                Keyword, func.count(Keyword.id), func.avg(PaperKeywords.score)
+            )
             .join(PaperKeywords)
             .join(Paper)
             .group_by(Keyword.id)
-            .order_by(func.count(Keyword.id).desc())
-            .having(func.count() >= no_below)
+            .order_by(func.avg(PaperKeywords.score).desc())
+            .having(func.count() >= self.no_below)
             .having(func.count() <= no_above_abs)
+            .having(func.avg(PaperKeywords.score) >= self.min_mean_score)
         )
-        limited_kwds = q2.all()
+        return kwd_query.all()
+
+    def get_tokens(self, session):
+        kwds = self.get_filtered_keywords(session)
+        q = session.query(Paper)
         tokens = (
-            [pk.keyword.keyword for pk in paper.keywords if pk.keyword in limited_kwds]
+            [pk.keyword.keyword for pk in paper.keywords if pk.keyword in kwds]
             for paper in q.all()
         )
         return tokens
 
-    def set_dictionary(self, session, no_below=5, no_above=0.5):
-        tokens = self.get_tokens(session, no_below, no_above)
+    def set_dictionary(self, session):
+        tokens = self.get_tokens(session)
         self.dct = Dictionary(tokens)
 
-    def set_corpus(self, session):
+    def get_corpus(self, session):
         return [self.dct.doc2bow(d) for d in self.get_tokens(session)]
 
 
@@ -197,6 +284,7 @@ def get_spacy_nlp():
     nlp = spacy.load("en_core_web_sm")
 
     # modify tokenizer infix patterns to not split on hyphen
+    # Need to get words like x-ray and gamma-ray
     infixes = (
         LIST_ELLIPSES
         + LIST_ICONS
@@ -224,8 +312,7 @@ def cli():
 
 @cli.command()
 @click.option("--db_loc", type=Path)
-@click.option("--normalize", type=str, default=None)
-def get_keywords_from_texts(db_loc, normalize):
+def get_keywords_from_texts(db_loc):
     engine = create_engine(f"sqlite:///{db_loc}")
 
     Session = sessionmaker(bind=engine)
@@ -233,16 +320,43 @@ def get_keywords_from_texts(db_loc, normalize):
 
     try:
         nlp = get_spacy_nlp()
-        pm = PaperMiner(nlp)
-        pm.extract_all_keywords(session, normalize)
+        pm = PaperKeywordExtractor(nlp)
+        pm.extract_all_keywords(session)
         session.commit()
-        LOG.info(f"Added extracted keywords to papers")
+        LOG.info(f"Added extracted keywords to papers.")
     except:
         LOG.warning(f"Aborted extracting keywords.")
         session.rollback()
         raise
     finally:
         session.close()
+
+
+@cli.command()
+@click.option("--db_loc", type=Path)
+def add_missed_locations(db_loc):
+    engine = create_engine(f"sqlite:///{db_loc}")
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        nlp = get_spacy_nlp()
+        pm = PaperKeywordExtractor(nlp)
+        records = pm.get_missed_keyword_locations(session, no_below=100, no_above=0.25)
+        session.commit()
+        LOG.info(f"Got records for PaperKeywords to be added.")
+    except:
+        LOG.warning(f"Aborted extracting keywords.")
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+    # Use engine instead of session for sake of speed.
+    # See https://docs.sqlalchemy.org/en/13/faq/performance.html#i-m-inserting-400-000-rows-with-the-orm-and-it-s-really-slow
+    engine.execute(PaperKeywords.__table__.insert(), records)
+    LOG.info(f"Added {len(records)} missed locations.")
 
 
 @cli.command()

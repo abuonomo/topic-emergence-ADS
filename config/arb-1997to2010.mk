f1d49bd60a2d8173afde8f3d4212dfe80daf4a1f
EC2 Default User <ec2-user@ip-172-31-57-130.ec2.internal>
2020-10-06 13:56:01 +0000

    add 1997to2010 experiment

---
+++ config/arb-1997to2010.mk
@@ -0,0 +1,7 @@
+EXPERIMENT_NAME=arb-1997to2010
+LIMIT=0
+BATCH_SIZE=10000
+N_PROCESS=10
+PARAM_YAML=config/arb-1997to2010.yaml
+PORT=5000
+INFO="Gensim corpus without keyword counts, just occurrence. Using updated blacklist as of 08/03/2020. Using 1997 to 2010."

e16b411b71804a263baffeb25563f6389e76eb39
Brian Thomas <brian.a.thomas@nasa.gov>
2020-10-19 16:21:32 +0000

    use updated keyword blacklist

--- config/arb-1997to2010.mk
+++ config/arb-1997to2010.mk
@@ -1,7 +1,6 @@
 EXPERIMENT_NAME=arb-1997to2010
 LIMIT=0
-BATCH_SIZE=10000
-N_PROCESS=10
+BATCH_SIZE=1000
 PARAM_YAML=config/arb-1997to2010.yaml
 PORT=5000
-INFO="Gensim corpus without keyword counts, just occurrence. Using updated blacklist as of 08/03/2020. Using 1997 to 2010."
+INFO="Only using 1997-2010 for topic modeling. Gensim corpus without keyword counts, just occurrence. Using updated blacklist as of 08/03/2020. Threshold 0.075"
